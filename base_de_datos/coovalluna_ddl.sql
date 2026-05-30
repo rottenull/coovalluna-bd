21e29bec112be6ab1f8d8ad1f566f3c5cc7d66ad
LAURA SOFIA ECHEVERRY GONZALEZ <echeverry.laura@correounivalle.edu.co>
2026-05-29 21:35:33 -0500

    Add DDL script for AGENCIA table

--- base_de_datos/coovalluna_ddl.sql
+++ base_de_datos/coovalluna_ddl.sql
@@ -3,6 +3,7 @@
 -- Base de Datos Relacionales 750006C - Gr. 52
 -- Período Académico Febrero - Junio 2026
 -- ============================================================
+
 CREATE TABLE AGENCIA (
 	CODIGO INT PRIMARY KEY,
 	NOMBRE VARCHAR(80) NOT NULL,
