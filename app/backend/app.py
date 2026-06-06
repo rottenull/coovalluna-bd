fa0df544cba02abe6b21ead48c41cc3f0aec483d
rottenull <echeverry.laura@correounivalle.edu.co>
2026-06-06 17:20:11 -0500

    Configuración de flask, variables de entorno y ruta inicial al login

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,14 +1,15 @@
-import os
 from flask import Flask, redirect, url_for
-from config import config
+from config import Config
 from routes.auth import auth_bp
 
-base_dir = os.path.dirname(os.path.dirname(__file__))
-template_dir = os.path.join(base_dir, 'frontend', 'templates')
-static_dir = os.path.join(base_dir, 'frontend', 'static')
+app = Flask(
+    __name__,
+    template_folder='../frontend/templates',
+    static_folder='../frontend/static'
+)
 
-app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
-app.config['secret_key'] = config.secret_key
+app.config['SECRET_KEY'] = Config.SECRET_KEY
+app.config['DEBUG'] = Config.DEBUG
 
 app.register_blueprint(auth_bp)
 
@@ -17,4 +18,4 @@ def inicio():
     return redirect(url_for('auth.login'))
 
 if __name__ == '__main__':
-    app.run(debug=config.debug)
+    app.run(debug=Config.DEBUG)
